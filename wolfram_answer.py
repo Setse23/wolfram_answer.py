216a0f6788e740d0cbb4663e901e912dbf1d43a4
Setse23 <47675993+Setse23@users.noreply.github.com>
2020-09-30 20:37:03 -0400

    Add files via upload

---
+++ wolfram_answer.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+key = '42QAWX-5A7QT5V695'
+question = str(input('Input the question you want Wolfram Alpha to answer > '))
+
+r = requests.get('https://api.wolframalpha.com/v2/query?input={}&appid={}&format=plaintext&output=json'.format(question.replace(' ', '+'), key))
+
+json_data = r.json()
+
+print(json_data['queryresult']['pods'][1]['subpods'][0]['plaintext'])
+
+"""Add it so that you can have show multiple solutions"""
